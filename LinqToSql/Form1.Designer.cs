d1fd78df08affc31c6edaac7643c8b232dcfe352
batuhan güneç <proffsar2@gmail.com>
2019-12-10 18:46:01 +0300

    Add project files.

---
+++ LinqToSql/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace LinqToSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSorgu5 = new System.Windows.Forms.Button();
+            this.btnSorgu6 = new System.Windows.Forms.Button();
+            this.btnSorgu7 = new System.Windows.Forms.Button();
+            this.btnSorgu8 = new System.Windows.Forms.Button();
+            this.btnSorgu1 = new System.Windows.Forms.Button();
+            this.btnSorgu3 = new System.Windows.Forms.Button();
+            this.btnSorgu2 = new System.Windows.Forms.Button();
+            this.btnSorgu4 = new System.Windows.Forms.Button();
+            this.btnSorgu9 = new System.Windows.Forms.Button();
+            this.btnSorgu11 = new System.Windows.Forms.Button();
+            this.btnSorgu10 = new System.Windows.Forms.Button();
+            this.btnSorgu12 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSorgu5
+            // 
+            this.btnSorgu5.Location = new System.Drawing.Point(25, 56);
+            this.btnSorgu5.Name = "btnSorgu5";
+            this.btnSorgu5.Size = new System.Drawing.Size(135, 39);
+            this.btnSorgu5.TabIndex = 0;
+            this.btnSorgu5.Text = "Find ID 5";
+            this.btnSorgu5.UseVisualStyleBackColor = true;
+            this.btnSorgu5.Click += new System.EventHandler(this.btnSorgu5_Click);
+            // 
+            // btnSorgu6
+            // 
+            this.btnSorgu6.Location = new System.Drawing.Point(181, 56);
+            this.btnSorgu6.Name = "btnSorgu6";
+            this.btnSorgu6.Size = new System.Drawing.Size(135, 39);
+            this.btnSorgu6.TabIndex = 0;
+            this.btnSorgu6.Text = "First";
+            this.btnSorgu6.UseVisualStyleBackColor = true;
+            this.btnSorgu6.Click += new System.EventHandler(this.btnSorgu6_Click);
+            // 
+            // btnSorgu7
+            // 
+            this.btnSorgu7.Location = new System.Drawing.Point(346, 56);
+            this.btnSorgu7.Name = "btnSorgu7";
+            this.btnSorgu7.Size = new System.Drawing.Size(135, 39);
+            this.btnSorgu7.TabIndex = 0;
+            this.btnSorgu7.Text = "FirstOrDefault";
+            this.btnSorgu7.UseVisualStyleBackColor = true;
+            this.btnSorgu7.Click += new System.EventHandler(this.btnSorgu7_Click);
+            // 
+            // btnSorgu8
+            // 
+            this.btnSorgu8.Location = new System.Drawing.Point(505, 56);
+            this.btnSorgu8.Name = "btnSorgu8";
+            this.btnSorgu8.Size = new System.Drawing.Size(135, 39);
+            this.btnSorgu8.TabIndex = 0;
+            this.btnSorgu8.Text = "Only Title Mr. And Age > 60";
+            this.btnSorgu8.UseVisualStyleBackColor = true;
+            this.btnSorgu8.Click += new System.EventHandler(this.btnSorgu8_Click);
+            // 
+            // btnSorgu1
+            // 
+            this.btnSorgu1.Location = new System.Drawing.Point(25, 11);
+            this.btnSorgu1.Name = "btnSorgu1";
+            this.btnSorgu1.Size = new System.Drawing.Size(135, 39);
+            this.btnSorgu1.TabIndex = 0;
+            this.btnSorgu1.Text = "Select, DateDiff, OrderByDescending";
+            this.btnSorgu1.UseVisualStyleBackColor = true;
+            this.btnSorgu1.Click += new System.EventHandler(this.btnSorgu1_Click);
+            // 
+            // btnSorgu3
+            // 
+            this.btnSorgu3.Location = new System.Drawing.Point(346, 11);
+            this.btnSorgu3.Name = "btnSorgu3";
+            this.btnSorgu3.Size = new System.Drawing.Size(135, 39);
+            this.btnSorgu3.TabIndex = 0;
+            this.btnSorgu3.Text = "Only Mr. And Dr.";
+            this.btnSorgu3.UseVisualStyleBackColor = true;
+            this.btnSorgu3.Click += new System.EventHandler(this.btnSorgu3_Click);
+            // 
+            // btnSorgu2
+            // 
+            this.btnSorgu2.Location = new System.Drawing.Point(181, 11);
+            this.btnSorgu2.Name = "btnSorgu2";
+            this.btnSorgu2.Size = new System.Drawing.Size(135, 39);
+            this.btnSorgu2.TabIndex = 0;
+            this.btnSorgu2.Text = "Where, DatePart";
+            this.btnSorgu2.UseVisualStyleBackColor = true;
+            this.btnSorgu2.Click += new System.EventHandler(this.btnSorgu2_Click);
+            // 
+            // btnSorgu4
+            // 
+            this.btnSorgu4.Location = new System.Drawing.Point(505, 11);
+            this.btnSorgu4.Name = "btnSorgu4";
+            this.btnSorgu4.Size = new System.Drawing.Size(135, 39);
+            this.btnSorgu4.TabIndex = 0;
+            this.btnSorgu4.Text = "All Get";
+            this.btnSorgu4.UseVisualStyleBackColor = true;
+            this.btnSorgu4.Click += new System.EventHandler(this.btnSorgu4_Click);
+            // 
+            // btnSorgu9
+            // 
+            this.btnSorgu9.Location = new System.Drawing.Point(25, 101);
+            this.btnSorgu9.Name = "btnSorgu9";
+            this.btnSorgu9.Size = new System.Drawing.Size(135, 39);
+            this.btnSorgu9.TabIndex = 0;
+            this.btnSorgu9.Text = "Any";
+            this.btnSorgu9.UseVisualStyleBackColor = true;
+            this.btnSorgu9.Click += new System.EventHandler(this.btnSorgu9_Click);
+            // 
+            // btnSorgu11
+            // 
+            this.btnSorgu11.Location = new System.Drawing.Point(346, 101);
+            this.btnSorgu11.Name = "btnSorgu11";
+            this.btnSorgu11.Size = new System.Drawing.Size(135, 39);
+            this.btnSorgu11.TabIndex = 0;
+            this.btnSorgu11.Text = "Skip";
+            this.btnSorgu11.UseVisualStyleBackColor = true;
+            this.btnSorgu11.Click += new System.EventHandler(this.btnSorgu11_Click);
+            // 
+            // btnSorgu10
+            // 
+            this.btnSorgu10.Location = new System.Drawing.Point(181, 101);
+            this.btnSorgu10.Name = "btnSorgu10";
+            this.btnSorgu10.Size = new System.Drawing.Size(135, 39);
+            this.btnSorgu10.TabIndex = 0;
+            this.btnSorgu10.Text = "Any And StartsWith";
+            this.btnSorgu10.UseVisualStyleBackColor = true;
+            this.btnSorgu10.Click += new System.EventHandler(this.btnSorgu10_Click);
+            // 
+            // btnSorgu12
+            // 
+            this.btnSorgu12.Location = new System.Drawing.Point(505, 101);
+            this.btnSorgu12.Name = "btnSorgu12";
+            this.btnSorgu12.Size = new System.Drawing.Size(135, 39);
+            this.btnSorgu12.TabIndex = 0;
+            this.btnSorgu12.Text = "UnitPrice >= 50";
+            this.btnSorgu12.UseVisualStyleBackColor = true;
+            this.btnSorgu12.Click += new System.EventHandler(this.btnSorgu12_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 146);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(642, 257);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSorgu4);
+            this.Controls.Add(this.btnSorgu12);
+            this.Controls.Add(this.btnSorgu8);
+            this.Controls.Add(this.btnSorgu2);
+            this.Controls.Add(this.btnSorgu10);
+            this.Controls.Add(this.btnSorgu6);
+            this.Controls.Add(this.btnSorgu3);
+            this.Controls.Add(this.btnSorgu11);
+            this.Controls.Add(this.btnSorgu7);
+            this.Controls.Add(this.btnSorgu1);
+            this.Controls.Add(this.btnSorgu9);
+            this.Controls.Add(this.btnSorgu5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSorgu5;
+        private System.Windows.Forms.Button btnSorgu6;
+        private System.Windows.Forms.Button btnSorgu7;
+        private System.Windows.Forms.Button btnSorgu8;
+        private System.Windows.Forms.Button btnSorgu1;
+        private System.Windows.Forms.Button btnSorgu3;
+        private System.Windows.Forms.Button btnSorgu2;
+        private System.Windows.Forms.Button btnSorgu4;
+        private System.Windows.Forms.Button btnSorgu9;
+        private System.Windows.Forms.Button btnSorgu11;
+        private System.Windows.Forms.Button btnSorgu10;
+        private System.Windows.Forms.Button btnSorgu12;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ LinqToSql/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.SqlServer;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqToSql
+{
+    public partial class Form1 : Form
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSorgu1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Employees.OrderByDescending(x => SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now)).Select(x => new {FirstName = x.FirstName,LastName = x.LastName,Title = x.Title,Age = SqlFunctions.DateDiff("Year",x.BirthDate,DateTime.Now) }).ToList();
+        }
+
+        private void btnSorgu2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Employees.Where(x => SqlFunctions.DatePart("Year",x.BirthDate) >= 1925 && SqlFunctions.DatePart("Year",x.BirthDate) <= 1958).ToList();
+        }
+
+        private void btnSorgu3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Employees.Where(x => x.TitleOfCourtesy == "Mr." || x.TitleOfCourtesy == "Dr.").ToList();
+        }
+
+        private void btnSorgu4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Categories.ToList();
+        }
+
+        private void btnSorgu5_Click(object sender, EventArgs e)
+        {
+            Employee employee = db.Employees.Find(5);
+            MessageBox.Show($"ID:{ employee.EmployeeID}\nFirst Name: { employee.FirstName}\nLast Name: {employee.LastName}" );
+        }
+
+        private void btnSorgu6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Category category = db.Categories.First(x => x.CategoryID < 12);
+                MessageBox.Show(category.CategoryName);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Böyle Bir ID Bulunmamaktadır");
+            }
+        }
+
+        private void btnSorgu7_Click(object sender, EventArgs e)
+        {
+            Employee employee = db.Employees.FirstOrDefault(x => x.EmployeeID < 15);
+            if (employee == null)
+            {
+                MessageBox.Show("Böyle Bir ID Bulunmamaktadır...");
+            }
+            else
+            {
+                MessageBox.Show(employee.FirstName);
+            }
+        }
+
+        private void btnSorgu8_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Employees.Where(x => SqlFunctions.DateDiff("YY", x.BirthDate, DateTime.Now) > 60).ToList();
+        }
+
+        private void btnSorgu9_Click(object sender, EventArgs e)
+        {
+            bool sonuc = db.Categories.Any(x => x.CategoryName == "Batuhan");
+            MessageBox.Show(sonuc.ToString());
+        }
+
+        private void btnSorgu10_Click(object sender, EventArgs e)
+        {
+            bool sonuc = db.Employees.Any(x => x.FirstName.StartsWith("Ce"));
+            MessageBox.Show(sonuc.ToString());
+        }
+
+        private void btnSorgu11_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Products.OrderByDescending(x => x.UnitPrice).Skip(3).Take(9).ToList();
+        }
+
+        private void btnSorgu12_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Products.Where(x => x.UnitPrice >= 50).OrderByDescending(x => x.UnitPrice).Select(x => new
+            {
+                ProductName = x.ProductName,
+                UnitPrice = x.UnitPrice,
+                UnitStock = x.UnitsInStock
+            }).ToList();
+        }
+    }
+}
